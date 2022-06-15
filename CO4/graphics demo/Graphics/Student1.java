0025a56f94be47688d8cfd95c34455525ef825cf
Sayeemasallu2000 <100747494+Sayeemasallu2000@users.noreply.github.com>
2022-06-15 11:51:05 +0530

    Add files via upload

---
+++ CO4/graphics demo/Graphics/Student1.java
@@ -0,0 +1,24 @@
+package Graphics;
+import java.io.*;
+import java.util.*;
+interface sp
+{
+final int score = 10;
+public void show();
+}
+public class Student1 implements sp
+{
+int rollno;
+String name;
+Scanner sc = new Scanner(System.in);
+public void get()
+{
+rollno = 20;
+name="Subha";
+}
+public void show()
+{
+System.out.println("Rollno,Name,Score: "+rollno +" "+name+" " +score);
+}
+}
+
