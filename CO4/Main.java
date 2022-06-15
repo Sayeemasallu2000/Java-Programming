8160fcf40032e8541034e398932afb7fe46c7197
Sayeemasallu2000 <100747494+Sayeemasallu2000@users.noreply.github.com>
2022-06-15 11:51:17 +0530

    Add files via upload

--- CO4/Main.java
+++ CO4/Main.java
@@ -1,66 +1,31 @@
 import java.util.*;
-class StackArr{
-int a[] = new int[530];
-int top=-1,ch,item,i;
-Scanner sc = new Scanner(System.in);
-public void stackoperation()
-{
-System.out.println("Enter the limit of elements in the stack : ");
-int n=sc.nextInt();
-do
-{
-System.out.println("\n\t CHOICES : ");
-System.out.println("\n 1.PUSH \n 2.POP \n 3.EXIT \n");
-System.out.println("\n Enter your choice : ");
-ch=sc.nextInt();
-switch(ch)
-{
-case 1: if(top >=n-1)
-{
-System.out.println("stack overflow");
-}
-else
-{
-System.out.println("enter the element :");
-item =sc.nextInt();
-top=top+1;
-a[top]=item;
-}
-break;
-case 2 : if(top<0)
-{
-System.out.println("stack underflow");
-}
-else
-  {
-a[top]='\0';
-top=top-1;
-}
-break;
-case 3 : break;
-default : System.out.println("\n Invalid choice");
-}
-if(top < 0)
-{
-System.out.println("\n stack is empty");
-}
-else
-{
-System.out.println("\n stack is \n");
-for(i=top;i>=0;i--)
-{
-System.out.println(a[i]);
+class Main {
+public static void main(String[] args) {
+
+HashSet<String> h_set1 = new HashSet<String>();
+
+h_set1.add("Red");
+h_set1.add("Green");
+h_set1.add("Black");
+h_set1.add("White");
+System.out.println("Frist HashSet : "+h_set1);
+HashSet<String>h_set2 = new HashSet<String>();
+h_set2.add("Red");
+h_set2.add("Pink");
+h_set2.add("Black");
+h_set2.add("Orange");
+System.out.println("Second HashSet : "+h_set2);
+Set<String> union = new HashSet<String>(h_set1);
+union.addAll(h_set2);
+System.out.println("Union:"+union);
+
+Set<String> intersection = new HashSet<String>(h_set1);
+intersection.retainAll(h_set2);
+System.out.println("Intersection:"+intersection);
+
+  Set<String> difference = new HashSet<String>(h_set1);
+difference.removeAll(h_set2);
+System.out.println("Difference:"+difference);
 }
 }
-}
-while(ch!=3);
-}
-}
-class Main
-{
-public static void main(String[] args)
-{
-StackArr sa =new StackArr();
-sa.stackoperation();
-}
-}
+
